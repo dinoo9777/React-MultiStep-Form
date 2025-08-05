99774b2c07ec8d1652ba391371a7df4224ff8969
dinesh <dinesh.yallurkar@gmail.com>
2025-08-05 17:23:57 +0530

    fix: configure HashRouter for GitHub Pages

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { FormProvider } from "./context/FormContext";
 import "./index.css";
+import { HashRouter } from "react-router-dom";
+
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <FormProvider>
-      <App />
-    </FormProvider>
+    <HashRouter>
+      <FormProvider>
+        <App />
+      </FormProvider>
+    </HashRouter>
   </React.StrictMode>
 );
